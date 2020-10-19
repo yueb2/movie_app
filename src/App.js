39a529c172af9c843a9cc5fc4590f76d1891b7a8
yueb2 <yueb2@naver.com>
2020-10-19 20:47:55 +0900

    component life cycle test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 //class component
 //state => object이고, component의 data를 넣을 공간이 있다. (data는 변한다. 즉, 바꾸고싶은 data를 state에 넣는다.)
@@ -9,7 +8,7 @@ class App extends React.Component {
   };
 
   //  this.setState({ count: this.state.count + 1 }) 대신 current를 사용한다. -> 위와같이 작성시, 어디선가 this.state.count가 update된다면 꼬일수 있다.
-  //setState를 호출하는 매순간 react는 새로운 state와 함깨ㅔ render function을 호출한다.
+  //setState를 호출하는 매순간 react는 새로운 state와 함깨 render function을 호출한다.
   add = () => {
     this.setState((current) => ({ count: current.count + 1 }));
   };
@@ -17,7 +16,17 @@ class App extends React.Component {
     this.setState((current) => ({ count: current.count - 1 }));
   };
 
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just Update!!");
+  }
+  componentWillUnmount() {
+    console.log("GoodBYe, cruel world");
+  }
   render() {
+    console.log("I'm rendering");
     return (
       <div>
         <h1>The number is : {this.state.count}</h1>

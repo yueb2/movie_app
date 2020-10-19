06b283d71a4f50e0ac6bf689dd8c0f67f52675c5
yueb2 <yueb2@naver.com>
2020-10-19 15:24:21 +0900

    how to use jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+//application에서 Food component로 보낸 모든속성을 가져와서, Food comonent의 argument로 속성을 넣는다.
+//props object내부에는 fav가 있다.
+//props.fav => {fav}와 동일하다. (ES6)
+function Food({fav}){
+  return <h1>I Like {fav}</h1>
+}
+//component : HTML을 반환하는 함수.
+//application에서 Food component에게 fav라는 이름의 prop(property)를 kimchi라는 value로 주었다.
+//react magic을 이용해 props 사용. 
+//react magic에서 react는 우리가 전달한 props를 가져가는 일을 한다.
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>hi!!!!</h1>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+//<App /> : component라고 부른다. React는 component와 함께 동작하고, 모든것은 component이다.
+//react는 component를 사용해서 HTML처럼 작성하려는 경우에 필요하다. javascript와 HTML사이의 이러한 조합을 jsx라고 부른다.
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();

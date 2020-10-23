9ff645e9bb50ac548a0f649fca12f39a07696c84
yueb2 <yueb2@naver.com>
2020-10-23 16:11:56 +0900

    fixed detail page & url

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,8 +1,23 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
-function Detail(props) {
-  console.log(props);
-  return <span> HI</span>;
+//클릭없이 url로 이동시 home으로 redirect
+class Detail extends React.Component {
+  componentDidMount() {
+    const { location, history } = this.props;
+    if (location.state === undefined) {
+      history.push("/");
+    }
+  }
+  //실행 순서 render -> componentDidMount
+  render() {
+    const { location } = this.props;
+    if (location.state) {
+      return <span>{location.state.title}</span>;
+    } else {
+      return null;
+    }
+  }
 }
 
 export default Detail;
